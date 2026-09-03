ac64d9711097c7e632474e43714ec8a7e1d3325d
binaryblob <sasasilaev27@gmail.com>
2026-09-03 07:39:15 +0500

    add constants for Thridix

--- source/shared/config.asm
+++ source/shared/config.asm
@@ -22,6 +22,16 @@ INITRIX_LOAD_OFFSET  equ 0
 KERNEL_LOAD_SEGMENT  equ 0x1000
 KERNEL_LOAD_OFFSET   equ 0
 KERNEL32_PHYS_ADDR   equ (KERNEL_LOAD_SEGMENT*16 + KERNEL_LOAD_OFFSET)
+THIRDIX_LOAD_SEGMENT equ 0x4000
+THIRDIX_LOAD_OFFSET  equ 0
+THIRDIX_PHYS_ADDR    equ (THIRDIX_LOAD_SEGMENT*16 + THIRDIX_LOAD_OFFSET)
+
+; CMOS-флаг "пропустить Thirdix", грузить 16-битное ядро сразу, пропуская Thirdix.
+; (bios-api/memory/cmos.asm).
+CMOS_FLAG_INDEX     equ 0x31
+CMOS_FLAG_CHECK     equ 0x32
+CMOS_FLAG_LRMK      equ 0xAA
+CMOS_MAGIC_CHECK    equ 0x55
 
 ; Раскладка структуры PCINFO
 PCINFO_ALL_MEM      equ 0   ; Размер всей памяти (МБ)
@@ -29,7 +39,21 @@ PCINFO_LOW_MEM      equ 4   ; Размер "нижней" памяти (КБ)
 PCINFO_MMAP_ENTRIES equ 6   ; Кол-во записей карты памяти
 PCINFO_DRIVE        equ 8   ; Номер загрузочного диска
 PCINFO_VIDEOMODE    equ 10  ; Номер видеорежима (0 - текстовый, 1 - видеорежим)
-PCINFO_MMAP         equ 12  ; Массив записей E820
+; --- VBE Framebuffer(bios-api/video/vbe.asm) ---
+PCINFO_FB_ADDR      equ 12  ; физ. адрес framebuffer (u32).
+PCINFO_FB_PITCH     equ 16  ; байт на строку (u16).
+PCINFO_FB_WIDTH     equ 18  ; Ширина в пикселях (u16).
+PCINFO_FB_HEIGHT    equ 20  ; Высота в пикселях (u16).
+PCINFO_FB_BPP       equ 22  ; Бит на пиксель (u8).
+PCINFO_FB_VALID     equ 23  ; 1 = framebuffer через Vesa BIOS Ext's, 0 = нет (u8).
+PCINFO_MMAP         equ 24  ; Массив записей E820, сдвинул на 24 из-за VBE.
+
+; VBE: желаемый режим и адреса временных буферов для BIOS-вызовов
+VBE_DESIRED_WIDTH   equ 1024
+VBE_DESIRED_HEIGHT  equ 768
+VBE_DESIRED_BPP     equ 32
+VBE_INFO_BUFFER     equ 0x6000
+VBE_MODEINFO_BUFFER equ 0x6200
 
 ; Маркеры кластеров FAT12 (общие для bootix и bios-api/fat12)
 CHAIN_END   equ 0x0FF8  ; Кластер >= этого - конец цепочки (EOF)
